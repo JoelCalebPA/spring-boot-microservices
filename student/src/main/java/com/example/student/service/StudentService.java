45e6ae4649f7c053bae2faf7919a6eaf46376c5d
Joel Paytan <joel.caleb.pa@gmail.com>
2023-10-03 09:35:12 -0500

    replicated and working

---
+++ student/src/main/java/com/example/student/service/StudentService.java
@@ -0,0 +1,28 @@
+package com.example.student.service;
+
+import com.example.student.model.Student;
+import com.example.student.repository.StudentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class StudentService {
+
+    private final StudentRepository repository;
+
+    public void saveStudent(Student student) {
+        repository.save(student);
+    }
+
+    public List<Student> findAllStudents() {
+        return repository.findAll();
+    }
+
+    public List<Student> findAllStudentsBySchool(Integer schoolId) {
+        return repository.findAllBySchoolId(schoolId);
+    }
+
+}
